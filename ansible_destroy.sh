3543821004dc412a20440c50cc9c17aa29b7f556
p017395 <jh_vs@hotmail.com>
2020-12-16 13:30:06 -0300

    cleaning

--- ansible_destroy.sh
+++ ansible_destroy.sh
@@ -1,2 +1,4 @@
-terraform destroy --auto-approve --target azurerm_linux_virtual_machine.vm_aula_ansible --target azurerm_storage_account.storage_aula_ansible
-terraform destroy --auto-approve --target azurerm_network_interface.nic_aula_ansible --target azurerm_public_ip.publicip_aula_ansible
+terraform destroy --auto-approve    --target azurerm_linux_virtual_machine.vm_aula_ansible \
+                                    --target azurerm_storage_account.storage_aula_ansible \
+                                    --target azurerm_network_interface.nic_aula_ansible \
+                                    --target azurerm_public_ip.publicip_aula_ansible
