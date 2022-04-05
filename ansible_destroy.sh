4c61cb4e12326120dfa905cc263d588f51e1b9f1
joaovictorino <jhvictorino@gmail.com>
2022-04-05 12:24:17 -0300

    update provider version

--- ansible_destroy.sh
+++ ansible_destroy.sh
@@ -1,3 +1,5 @@
+terraform init -upgrade
+
 terraform destroy --auto-approve    --target azurerm_linux_virtual_machine.vm_aula_ansible \
                                     --target azurerm_storage_account.storage_aula_ansible \
                                     --target azurerm_network_interface.nic_aula_ansible \
