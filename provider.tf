12475265eb08d37f8002ccdca39aee4a68ef65c8
joaovictorino <jhvictorino@gmail.com>
2023-10-30 23:06:50 -0300

    azure rm version

--- provider.tf
+++ provider.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 0.13"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.25.0"
+      version = "= 3.58.0"
     }
   }
 }
